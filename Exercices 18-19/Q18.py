fc877a3fe76b4b7172c546cfe9484982f6133de7
Naim-Ben-Alaya <98695786+Benalayanaim@users.noreply.github.com>
2023-11-20 09:25:02 +0100

    Add files via upload

---
+++ Exercices 18-19/Q18.py
@@ -0,0 +1,63 @@
+def is_low(x):                  # Returns True  if the string has a lowercase
+    for i in x:
+        if 'a'<=i and i<='z':
+            return True
+    return  False
+
+def is_up(x):                   # Returns True  if the string has a uppercase
+    for i in x:
+        if 'A'<= i and i<='Z':
+            return True
+    return  False
+
+def is_num(x):                  # Returns True  if the string has a numeric digit
+    for i in x:
+        if '0'<=i and i<='9':
+            return True
+    return  False
+
+def is_other(x):                # Returns True if the string has any "$#@"
+    for i in x:
+        if i=='$' or i=='#' or i=='@':
+            return True
+    return False
+
+s = input().split(',')
+lst = []
+
+for i in s:
+    length = len(i)
+    if 6 <= length and length <= 12 and is_low(i) and is_up(i) and is_num(i) and is_other(i):   #Checks if all the requirments are fulfilled
+        lst.append(i)
+
+print(",".join(lst))
+
+
+
+
+
+#Another Solutiion 
+
+def check(x):
+    cnt = (6<=len(x) and len(x)<=12)
+    for i in x:
+        if i.isupper():
+            cnt+=1
+            break
+    for i in x:
+        if i.islower():
+            cnt+=1
+            break
+    for i in x:
+        if i.isnumeric():
+            cnt+=1
+            break
+    for i in x:
+        if i=='@' or i=='#'or i=='$':
+            cnt+=1
+            break
+    return cnt == 5               # counting if total 5 all conditions are fulfilled then returns True
+
+s = input().split(',')
+lst = filter(check,s)             # Filter function pick the words from s, those returns True by check() function
+print(",".join(lst))
